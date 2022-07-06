4778d3e96084ea1d68682ccc4910955f5d03e4a1
TERI A PETERS <teriapeters@TERIs-MacBook-Air.local>
2022-07-06 16:08:38 -0600

    Save schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
--- Creating tables for PH-EmployeeDB
-
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
@@ -7,14 +5,19 @@ CREATE TABLE departments (
 	UNIQUE (dept_name)
 );
 
+DROP TABLE employees;
+
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+	emp_no INT PRIMARY KEY,
 	birth_date DATE NOT NULL,
-	first_name varchar NOT NULL,
-	last_name varchar NOT NULL,
-	PRIMARY KEY (emp_no)
-	);
-	
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+SELECT * FROM employees;
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
@@ -24,31 +27,34 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+SELECT * FROM dept_manager;
 
-CREATE TABLE salaries(
+CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_date date NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp(
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (emp_no) 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE titles(
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles;
