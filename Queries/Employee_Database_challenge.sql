b79380a6a22b8c32d9191e3e3a4b9921cb868bf9
TERI A PETERS <teriapeters@TERIs-MacBook-Air.local>
2022-07-07 21:52:56 -0600

    Redo Deliverable 2 because I didn't save sql code.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,3 +29,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- Create new Mentorship Eligibility table and export csv file.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e 
+	INNER JOIN dept_emp as de 
+		ON (e.emp_no=de.emp_no)
+	INNER JOIN titles as t 
+		ON (e.emp_no=t.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
